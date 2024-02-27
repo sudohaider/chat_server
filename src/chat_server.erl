f0877095cb3d54d0986c07fbcfe169836b8be24e
Haider <ali.haider9211@gmail.com>
2024-02-27 02:55:09 +0000

    erlang_project_setup

---
+++ src/chat_server.erl
@@ -0,0 +1,8 @@
+%% File: src/chat_server.erl
+-module(chat_server).
+-export([start/0]).
+
+start() ->
+    %% Your code to start the chat server goes here
+    io:format("Chat server started.~n").
+
