1955be1bc4e6483e6ac952e4263c1299bcfd90b0
Haider <ali.haider9211@gmail.com>
2024-02-28 18:13:56 +0000

    README and server script modified for multiple_connections, deleted 'test' folder

--- test/chat_server_test.erl
+++
@@ -1,9 +0,0 @@
--module(chat_server_test).
--include_lib("eunit/include/eunit.hrl").
-
-start_chat_server_test() ->
-    chat_server:start().
-
-stop_chat_server_test() ->
-    chat_server:stop().
-
